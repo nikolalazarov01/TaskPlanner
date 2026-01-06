1f328c81cd465e4b18ee93303fce856b85074cd2
Nikola Lazarov <huko2009@gmail.com>
2026-01-06 18:01:35 +0200

    Changed the base api url

--- Client/src/services/api.ts
+++ Client/src/services/api.ts
@@ -9,7 +9,7 @@ import type {
   TaskResponseModel,
 } from '../types';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5099/api';
+const API_BASE_URL = 'https://localhost:44358/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
